37a3cc6e492806cc13d9f84348a2238b342a2544
shady-mungai <shadrackmungai10@gmail.com>
2023-03-16 17:08:41 +0300

    Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    
+    validate :sufficiently_clickbaity_title
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    def sufficiently_clickbaity_title
+        if title.present? && !title.match(/Won't Believe|Secret|Top \d+|Guess/)
+          errors.add(:title, 'must be sufficiently clickbait-y')
+        end
+      end
 end
